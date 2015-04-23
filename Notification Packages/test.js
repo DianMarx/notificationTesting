777a3735bb5e3334a159439dd7b0e5a2bd184973
Hanrich <Hanrichp@gmail.com>
2015-04-23 12:08:37 +0200

    testing notification B

--- Notification Packages/test.js
+++ Notification Packages/test.js
@@ -5,9 +5,11 @@
 // Notification A
 console.log("Testing integratability");
 
-var notification = require('./notification.js');
+
 var notification = require('unit.js');
 
 // Notification B
+
+var notification = require('./Notification\ B/DeleteNotif.js');
 
 
