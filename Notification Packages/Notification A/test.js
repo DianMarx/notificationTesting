1b2a08ae7125f02a04295afe0594bc45216f0a87
Hanrich <Hanrichp@gmail.com>
2015-04-23 11:52:00 +0200

    Added Notification B git hub clone

---
+++ Notification Packages/Notification A/test.js
@@ -0,0 +1,13 @@
+
+// Testing integratability
+// H.Potgieter 
+// Date 23 April 2015 
+// Notification A
+console.log("Testing integratability");
+
+var notification = require('./notification.js');
+var notification = require('unit.js');
+
+// Notification B
+
+
