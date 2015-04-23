2b8029678df24d9e2b439b99abdce371121e2cb5
Hanrich <Hanrichp@gmail.com>
2015-04-23 12:28:39 +0200

    Latex main file contains error. Added some more comments to my section

--- Notification Packages/test.js
+++ Notification Packages/test.js
@@ -2,13 +2,13 @@
 // Testing integratability
 // H.Potgieter 
 // Date 23 April 2015 
-// Notification A
+// Notification A integratability
 console.log("Testing integratability");
 var notification = require('./Notification\ A/notification.js');
 var notification = require('unit.js');
 
-// Notification B
-
-//var notification = require('./Notification\ B/DeleteNotif.js');
+// Notification B integratability
+//Their attempt at unit test fails....No way to get dependencys
+//var notification = require('./Notification\ B/AllTogether.js');
 
 
