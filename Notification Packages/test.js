a063fba67ab7eb3866f2b4e550216d9c905f177c
Hanrich <Hanrichp@gmail.com>
2015-04-23 12:25:28 +0200

    Latex main file contains error. Added some more comments to my section

--- Notification Packages/test.js
+++ Notification Packages/test.js
@@ -4,12 +4,11 @@
 // Date 23 April 2015 
 // Notification A
 console.log("Testing integratability");
-
-
+var notification = require('./Notification\ A/notification.js');
 var notification = require('unit.js');
 
 // Notification B
 
-var notification = require('./Notification\ B/DeleteNotif.js');
+//var notification = require('./Notification\ B/DeleteNotif.js');
 
 
