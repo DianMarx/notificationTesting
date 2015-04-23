42969e5a8cf5507782d96639d239ca89dfa03866
Hanrich <Hanrichp@gmail.com>
2015-04-23 12:55:08 +0200

    Added new modules

--- Notification Packages/test.js
+++ Notification Packages/test.js
@@ -9,6 +9,7 @@ var notification = require('unit.js');
 
 // Notification B integratability
 //Their attempt at unit test fails....No way to get dependencys
+// Please take a look at their "unit tests"
 var notification = require('./Notification\ B/test.js');
 
 
